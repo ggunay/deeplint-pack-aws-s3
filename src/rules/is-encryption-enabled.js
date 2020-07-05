008100130ad2df49c49f1bedc32c98e69dfdcc3e
Gavin Yue <yue.yuanyuan@gmail.com>
2020-07-04 20:31:07 -0700

    Add server side encryption check

--- src/rules/is-encryption-enabled.js
+++ src/rules/is-encryption-enabled.js
@@ -4,9 +4,9 @@ exports.check = async function (context) {
     for (const key of Object.keys(resources)) {
         for (const resource of resources[key]) {
             if(resource.type === 'aws::s3::bucket') {
-                if(resource.properties.VersioningConfiguration && ('Status' in resource.properties.VersioningConfiguration)) {
+                if(resource.properties.BucketEncryption && Object.keys(resource.properties.BucketEncryption).length === 0) {
                     problems.push({
-                        message: `AWS S3 Bucket: ${resource.name} does not enable versioning`
+                        message: `AWS S3 Bucket: ${resource.name} does not enable server-side encryption`
                     })
                 }
 
